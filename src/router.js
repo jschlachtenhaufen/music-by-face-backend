474968da0a322f898a2f24a6f3abe5514422e31c
John Schlachtenhaufen <jschlach25@gmail.com>
2019-08-17 16:17:53 -0500

    endpoint for genres and song rec

--- src/router.js
+++ src/router.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import spotify from './services/spotify';
 
 const router = Router();
+
+router.get('/spotify/genres', spotify.getToken, spotify.getGenres);
+router.get('/spotify/recommendation', spotify.getToken, spotify.getRecommendation);
 
 export default router;
---
+++ src/services/spotify.js
@@ -0,0 +1,87 @@
+import dotenv from 'dotenv';
+import rp from 'request-promise';
+
+const TOKEN_URL = 'https://accounts.spotify.com/api/token';
+const SEARCH_URL = 'https://api.spotify.com/v1/recommendations';
+dotenv.config({ silent: true });
+
+const spotify = {
+    token: {
+        value: '',
+        expiration: '',
+    },
+
+    _isTokenValid: () => {
+        if (!spotify.token.value) return false;
+        if (Date.now() / 1000 >= spotify.token.expiration - 300) return false;
+        return true;
+    },
+
+    // middleware function to pass on oauth token
+    getToken: (req, res, next) => {
+        if (spotify._isTokenValid()) {
+            next(spotify.token.value);
+        } else {
+            const { CLIENT_ID, CLIENT_SECRET } = process.env;
+            const auth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
+
+            const options = {
+                method: 'POST',
+                uri: TOKEN_URL,
+                form: { grant_type: 'client_credentials' },
+                headers: { Authorization: `Basic ${auth}` },
+                json: true,
+            };
+
+            rp(options).then((token) => {
+                spotify.token.value = token.access_token;
+                const currentTime = new Date();
+                spotify.token.expiration = currentTime / 1000 + token.expires_in;
+                next(spotify.token.value);
+            }).catch((err) => {
+                res.send(err);
+            });
+        }
+    },
+
+    getGenres: (token, req, res, next) => {
+        const options = {
+            method: 'GET',
+            uri: `${SEARCH_URL}/available-genre-seeds`,
+            headers: { Authorization: `Bearer ${token}` },
+            json: true,
+        };
+
+        rp(options).then((genres) => {
+            res.send(genres);
+        }).catch((err) => {
+            res.send(err);
+        });
+    },
+
+    getRecommendation: (token, req, res, next) => {
+        const options = {
+            method: 'GET',
+            uri: `${SEARCH_URL}`,
+            headers: { Authorization: `Bearer ${token}` },
+            json: true,
+            qs: {
+                seed_genres: req.genre,
+                target_energy: req.energy,
+                target_acousticness: req.acousticness,
+                target_danceability: req.danceability,
+                target_valence: req.valence,
+                limit: 1,
+                mode: req.mode,
+            },
+        };
+
+        rp(options).then((track) => {
+            res.send(track);
+        }).catch((err) => {
+            res.send(err);
+        });
+    },
+};
+
+export default spotify;
